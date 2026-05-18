2dfb0bc47fd5a4c52aa6e10287a4df73bf8cc006
EmillyMarrocos <emillymarrocos5@gmail.com>
2026-05-18 15:59:39 -0300

    Primeiro commit

---
+++ README.md.bak
@@ -0,0 +1,2 @@
+# Financeiro-API
+Desafio técnico para processo seletivo | API RESTful de controle de finanças pessoais - NestJS + TypeScript + MySQL
---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get<string>('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
+        username: config.get<string>('DB_USER'),
+        password: config.get<string>('DB_PASSWORD'),
+        database: config.get<string>('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true, // apenas em desenvolvimento
+      }),
+    }),
+  ],
+})
+export class AppModule {}
